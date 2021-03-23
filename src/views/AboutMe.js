6c14587272fe86239669fc6577137ff30260cfae
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-22 23:45:03 -0700

    set up profile page

--- src/views/AboutMe.js
+++ src/views/AboutMe.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+// reactstrap components
+import { Button, Card, Container, Row, Col } from "reactstrap";
+
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
+import profile from "assets/img/portfolio/profile-pic.png";
 
 class AboutMe extends React.Component {
-  state = {};
   componentDidMount() {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
@@ -15,8 +18,114 @@ class AboutMe extends React.Component {
     return (
       <>
         <DemoNavbar />
+        <main className="profile-page" ref="main">
+          <section className="section-profile-cover section-shaped my-0">
+            {/* Circles background */}
+            <div className="shape shape-style-1 full-background-img alpha-4">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            {/* SVG separator */}
+            <div className="separator separator-bottom separator-skew">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+          </section>
+          <section className="section">
+            <Container>
+              <Card className="card-profile shadow mt--300">
+                <div className="px-4">
+                  <Row className="justify-content-center">
+                    <Col className="order-lg-2" lg="3">
+                      <div className="card-profile-image">
+                        <a href="#pablo" onClick={e => e.preventDefault()}>
+                          <img
+                            alt="..."
+                            className="rounded-pill"
+                            src={profile}
+                          />
+                        </a>
+                      </div>
+                    </Col>
+                    <Col
+                      className="order-lg-3 text-lg-right align-self-lg-center"
+                      lg="4"
+                    >
+                    </Col>
+                    <Col className="order-lg-1" lg="4">
+                      <div className="card-profile-stats d-flex justify-content-center">
+                        <div>
+                          <span className="heading"></span>
+                          <span className="description"></span>
+                        </div>
+                        <div>
+                          <span className="heading"></span>
+                          <span className="description"></span>
+                        </div>
+                        <div>
+                          <span className="heading"></span>
+                          <span className="description"></span>
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
+                  <div className="text-center mt-7">
+                    <h3>
+                      Mason Aviles
+                    </h3>
+                    <div className="h6 font-weight-300">
+                      <i className="ni location_pin mr-2" />
+                      Seattle, WA
+                    </div>
+                    <div className="h6 mt-4">
+                      <i className="ni business_briefcase-24 mr-2" />
+                      Software Engineer
+                    </div>
+                    {/* <div>
+                      <i className="ni education_hat mr-2" />
+                      University of Computer Science
+                    </div> */}
+                  </div>
+                  <div className="mt-5 py-5 border-top text-center">
+                    <Row className="justify-content-center">
+                      <Col lg="9">
+                        <p>
+                          An artist of considerable range, Ryan — the name taken
+                          by Melbourne-raised, Brooklyn-based Nick Murphy —
+                          writes, performs and records all of his own music,
+                          giving it a warm, intimate feel with a solid groove
+                          structure. An artist of considerable range.
+                        </p>
+                        {/* <a href="#pablo" onClick={e => e.preventDefault()}>
+                          Show more
+                        </a> */}
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Card>
+            </Container>
+          </section>
+        </main>
+        <SimpleFooter />
       </>
-    )
+    );
   }
 }
 
